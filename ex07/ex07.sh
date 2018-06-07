9c6b68942f0a67588a07d1ca1383b67003143c52
chumaknadya <chumaknadya19@gmail.com>
2018-06-07 17:54:06 +0300

    7

---
+++ ex07/ex07.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+file="resourses/surnames.txt"
+count=0
+arg=$1
+re='Q-Chem'
+if ! [[ $arg ]]; then
+  exit 1
+fi
+
+if [[ $arg == 0  ]];then
+  exit 1
+fi
+while IFS= read line
+do
+  if [[ $count == $arg ]]; then
+    break
+  fi
+  let count+=1
+  if !  [[ $line =~ "$re" ]]; then
+    echo $line | sed 's/\.//' | sed 's/\-//'
+  fi
+done <"$file"
+
